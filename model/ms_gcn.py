47b27b9a008326c469d8305083011428b26b1a82
steveliao93 <43572350+steveliao93@users.noreply.github.com>
2021-10-20 10:27:02 +0100

    Update ms_gcn.py

--- model/ms_gcn.py
+++ model/ms_gcn.py
@@ -7,56 +7,50 @@ import torch.nn.functional as F
 import numpy as np
 
 from graph.tools import k_adjacency, normalize_adjacency_matrix
-from model.mlp import MLP
 from model.activation import activation_factory
 
 
-class MultiScale_GraphConv(nn.Module):
+class GraphConv(nn.Module):
     def __init__(self,
                  num_scales,
                  in_channels,
                  out_channels,
                  A_binary,
-                 disentangled_agg=True,
-                 use_mask=True,
-                 dropout=0,
                  activation='relu'):
         super().__init__()
         self.num_scales = num_scales
 
-        if disentangled_agg:
-            A_powers = [k_adjacency(A_binary, k, with_self=True) for k in range(num_scales)]
-            A_powers = np.concatenate([normalize_adjacency_matrix(g) for g in A_powers])
-        else:
-            A_powers = [A_binary + np.eye(len(A_binary)) for k in range(num_scales)]
-            A_powers = [normalize_adjacency_matrix(g) for g in A_powers]
-            A_powers = [np.linalg.matrix_power(g, k) for k, g in enumerate(A_powers)]
-            A_powers = np.concatenate(A_powers)
+        A_powers = [k_adjacency(A_binary, k, with_self=True) for k in range(num_scales)]
+        A_powers = np.concatenate([normalize_adjacency_matrix(g) for g in A_powers])
 
         self.A_powers = torch.Tensor(A_powers)
-        self.use_mask = use_mask
-        if use_mask:
-            # NOTE: the inclusion of residual mask appears to slow down training noticeably
-            self.A_res = nn.init.uniform_(nn.Parameter(torch.Tensor(self.A_powers.shape)), -1e-6, 1e-6)
+        self.A_res = nn.init.uniform_(nn.Parameter(torch.Tensor(self.A_powers.shape)), -1e-6, 1e-6)
 
-        self.mlp = MLP(in_channels * num_scales, [out_channels], dropout=dropout, activation=activation)
+        self.mlp = MLP(in_channels * num_scales, [out_channels], activation=activation)
 
     def forward(self, x):
         N, C, T, V = x.shape
         self.A_powers = self.A_powers.to(x.device)
         A = self.A_powers.to(x.dtype)
-        if self.use_mask:
-            A = A + self.A_res.to(x.dtype)
+        A = A + self.A_res.to(x.dtype)
         support = torch.einsum('vu,nctu->nctv', A, x)
         support = support.view(N, C, T, self.num_scales, V)
         support = support.permute(0,3,1,2,4).contiguous().view(N, self.num_scales*C, T, V)
         out = self.mlp(support)
         return out
 
+class MLP(nn.Module):
+    def __init__(self, in_channels, out_channels, activation='relu'):
+        super().__init__()
+        channels = [in_channels] + out_channels
+        self.layers = nn.ModuleList()
+        for i in range(1, len(channels)):
+            self.layers.append(nn.Conv2d(channels[i-1], channels[i], kernel_size=1))
+            self.layers.append(nn.BatchNorm2d(channels[i]))
+            self.layers.append(activation_factory(activation))
 
-if __name__ == "__main__":
-    from graph.ntu_rgb_d import AdjMatrixGraph
-    graph = AdjMatrixGraph()
-    A_binary = graph.A_binary
-    msgcn = MultiScale_GraphConv(num_scales=15, in_channels=3, out_channels=64, A_binary=A_binary)
-    msgcn.forward(torch.randn(16,3,30,25))
+    def forward(self, x):
+        # Input shape: (N,C,T,V)
+        for layer in self.layers:
+            x = layer(x)
+        return x
